722d13316929628e53fe5f56bcd534a7862606f8
S.P. Mohanty <spmohanty91@gmail.com>
2020-04-23 06:39:05 +0200

    Add links to contrib algorithm implementations

--- algorithms/registry.py
+++ algorithms/registry.py
@@ -1,11 +1,22 @@
 """
 Registry of custom implemented algorithms names
+
+Please refer to the following examples to add your custom algorithms : 
+
+- AlphaZero : https://github.com/ray-project/ray/tree/master/rllib/contrib/alpha_zero
+- bandits : https://github.com/ray-project/ray/tree/master/rllib/contrib/bandits
+- maddpg : https://github.com/ray-project/ray/tree/master/rllib/contrib/maddpg
+- random_agent: https://github.com/ray-project/ray/tree/master/rllib/contrib/random_agent
+
+An example integration of the random agent is shown here : 
+- https://github.com/AIcrowd/neurips2020-procgen-starter-kit/tree/master/algorithms/custom_random_agent
 """
+
 
 def _import_custom_random_agent():
     from .custom_random_agent.custom_random_agent import CustomRandomAgent
 
 
 CUSTOM_ALGORITHMS = {
-    "custom/CustomRandomAgent" : _import_custom_random_agent
+    "custom/CustomRandomAgent": _import_custom_random_agent
 }
