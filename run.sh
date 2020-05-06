f87fe0149ebfd536313f9b7524e4783a79b126e4
Jyotish <hypothesis1996@gmail.com>
2020-05-06 07:42:47 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,11 @@
 python train.py -f experiments/procgen-0.yaml --local-dir ${OUTPUTS_DIR}
 STATUS_CODE=$?
 
-mv ray_results ${OUTPUTS_DIR}/ray
+ls
+
+ls /tmp
+
+mv /tmp/ray_results/procgen ${OUTPUTS_DIR}/ray
 mv mlflow ${OUTPUTS}/mlflow
 
 exit $STATUS_CODE
