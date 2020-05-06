55fa936cc7af61a5e35c6722bb537f737e1f40a5
Jyotish <hypothesis1996@gmail.com>
2020-05-06 10:11:07 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python train.py -f experiments/procgen-0.yaml --local-dir ${OUTPUTS_DIR}
+python train.py -f experiments/procgen-0.yaml --ray-memory 1500000000 --ray-num-cpus 2 --ray-object-store-memory 1000000000
 STATUS_CODE=$?
 
 sleep 100000
