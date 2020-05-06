c2477a866ba032a74f543b0d8e04a7f76315c3af
Jyotish <hypothesis1996@gmail.com>
2020-05-06 10:27:57 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 python train.py -f experiments/procgen-0.yaml --ray-memory 1500000000 --ray-num-cpus 2 --ray-object-store-memory 1000000000
 STATUS_CODE=$?
 
-mv ray_results/procgen ${OUTPUTS_DIR}/ray
+mv ray_results/* ${OUTPUTS_DIR}/ray
 mv mlruns ${OUTPUTS}/mlruns
 
 exit $STATUS_CODE
